6cb8293e19bbcbd6d50767129e7693c6e7c8fdd4
Shaorya23 <117918115+Shaorya23@users.noreply.github.com>
2024-01-22 23:14:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+{
+    "token": "",
+    "proxy": true,
+    "nuke": {
+        "channels_name": [
+            "STEVE x SOMNATH",
+            "maa chud gyi baccho ki",
+            "somnath papa was here",
+            "fucked by steve papa"
+        ],
+        "roles_name": [
+            "Fucked By SP1IT",
+            "Or Kro Bakchodi",
+            "Somnath x Steve Papa"
+        ],
+        "messages_content": [
+            "@everyone @here **Fucked by Steve x Somnath** https://discord.gg/sp1it",
+            "@everyone @here **MUUT DIYA SERVER PE** https://discord.gg/sp1it",
+            "@everyone @here **SP1IT On Top Kidz** https://discord.gg/sp1it"
+        ]
+    }
+}
